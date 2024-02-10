6886b73aea449ff7f45beb306c919788318889f4
AFx3 <waxnwaxofficial@gmail.com>
2024-02-10 10:45:02 +0100

    better test cases

--- test/test.js
+++ test/test.js
@@ -33,8 +33,7 @@ var merkleTreeLevels = [];
 contract("Compute gas opponent", (accounts) => {
 
     let battleship;
-    const playerX = accounts[0];
-    const playerY = accounts[1];
+    const [playerX, playerY] = accounts;
     const data = {};
   
     before(async () => {
@@ -42,7 +41,7 @@ contract("Compute gas opponent", (accounts) => {
     });
   
     describe("Report opponent and wait match end", () => {
-      const amount = 100000;
+      const amount = 1000;
       let matchId;
   
       before(async () => {
@@ -50,26 +49,26 @@ contract("Compute gas opponent", (accounts) => {
         matchId = tx.logs[0].args._assignedMatchID;
       });
   
-      it("Join the match", async () => {
+      it("Join match", async () => {
         await battleship.JoinMatch(matchId, { from: playerY });
       });
   
-      it("Commit stake for the match", async () => {
+      it("Commit stake", async () => {
         await battleship.proposeStake(matchId, amount, { from: playerX });
         await battleship.acceptStake(matchId, { from: playerY });
       });
   
-      it("Pay stake to the contract", async () => {
+      it("Stake to contract", async () => {
         await battleship.payStake(matchId, { from: playerX, value: amount });
         await battleship.payStake(matchId, { from: playerY, value: amount });
       });
   
-      it("Accuse opponent of having left the match", async () => {
+      it("Accuse opponent left match", async () => {
         const tx = await battleship.accuseOpponent(matchId, { from: playerX });
         data.accuseOpponent = tx.receipt.gasUsed;
       });
   
-      it("Wait for five blocks", async () => {
+      it("Wait 5 blocks", async () => {
         for (let i = 0; i < 5; i++) {
           await battleship.NewMatch(board.size, board.shipNumber, { from: playerX });
         }
@@ -77,18 +76,17 @@ contract("Compute gas opponent", (accounts) => {
         data.accuseOpponent = tx.receipt.gasUsed;
       });
   
-      it("Save data to gas_total_cost.json", () => {
+      it("Save gas cost", () => {
         const prev = JSON.parse(fs.readFileSync(gasFile));
         fs.writeFileSync(gasFile, JSON.stringify({ ...data, ...prev }));
       });
     });
   });
   
-
-contract("Compute gas cost for a 8x8 board game", (accounts) => {
+// Contract testing for computing gas cost for an 8x8 board game
+contract("Compute gas cost", (accounts) => {
   let matchId;
-  const playerX = accounts[0];
-  const playerY = accounts[1];
+  const [playerX, playerY] = accounts;
   let battleship;
   const data = {};
 
@@ -98,12 +96,12 @@ contract("Compute gas cost for a 8x8 board game", (accounts) => {
 
   before(async () => {
     // Create game
-    const tx = await battleship.NewMatch(board.size, board.shipNumber,{
-      from: playerX,
-    });
+    const tx = await battleship.NewMatch(board.size, board.shipNumber, {from: playerX,});
     matchId = tx.logs[0].args._assignedMatchID;
 
     data.NewMatch = tx.receipt.gasUsed;
+
+    // check if the event is fired
     truffleAssert.eventEmitted(tx, "newMatchCreated", (ev) => {
       matchId = ev._assignedMatchID;
       return ev._proposer == playerX;
@@ -127,7 +125,7 @@ contract("Compute gas cost for a 8x8 board game", (accounts) => {
       data.JoinRandom = tx.receipt.gasUsed;
     });
 
-    const amount = 100000;
+    const amount = 1000;
     it("Propose a stake", async () => {
        
 
@@ -135,13 +133,13 @@ contract("Compute gas cost for a 8x8 board game", (accounts) => {
       data.proposeStake = tx.receipt.gasUsed;
     });
 
-    it("Opponent agree on proposed stake", async () => {
+    it("Opponent agree", async () => {
       const tx =  await battleship.acceptStake(matchId, { from: playerY });
 
       data.acceptStake = tx.receipt.gasUsed;
     });
 
-    it("Players deposit stake to the contract", async () => {
+    it("Stake to the contract", async () => {
       const tx =  await battleship.payStake(matchId, { from: playerX, value: amount });
 
       data.payStake = tx.receipt.gasUsed;
@@ -151,7 +149,7 @@ contract("Compute gas cost for a 8x8 board game", (accounts) => {
 
     let p1_treeRoot;
     let p2_treeRoot;
-    it("Players commit their board by registering Merkle Root", async () => {
+    it("Registering Merkle Root players", async () => {
   
       salt = Math.floor(Math.random() * board.size);
       p1_treeRoot = merkleTree(board,salt);
@@ -168,62 +166,36 @@ contract("Compute gas cost for a 8x8 board game", (accounts) => {
     
 
     it("Players attack each other", async () => {
-      // Player who joined the game always starts attacking first
-      
-    
-      //Generate 8x8 shoot combination
-      let finish = false;
+        let finish = false;
         for (let i = 0; i < 8 && !finish; i++) {
             for (let j = 0; j < 8 && !finish; j++){
-
-              try{
-                txAttack =  await battleship.attackOpponent(matchId, i, j, { from: playerX });
-                data.attackOpponent = txAttack.receipt.gasUsed;
-
-                var merkleProof = genMerkleProof(board, i, j);
-                let flatIndex = i * board.size + j;
-
-                txAttackProof = await battleship.submitAttackProof(matchId, String(board.cells[i][j]), String(merkleTreeLevels[0][flatIndex]), merkleProof, { from: playerY });
-                data.submitAttackProof = txAttackProof.receipt.gasUsed;
-
-         
-                txAttack =  await battleship.attackOpponent(matchId, i, j, { from: playerY });
-                data.attackOpponent = txAttack.receipt.gasUsed;
-
-                var merkleProof = genMerkleProof(board,i, j);
-                txAttackProof = await battleship.submitAttackProof(matchId, String(board.cells[i][j]), String(merkleTreeLevels[0][flatIndex]), merkleProof, { from: playerX });
-                data.submitAttackProof = txAttackProof.receipt.gasUsed;
-              }catch(error){
-              
-               //As the game is over, we can't attack anymore
-               //The `error` is generated by another attack perfomed when the `matchFinished` is already fired
-               //so to avoid listening to the event twice, we just break the loop
-               finish = true;
-               break;
-             
-            }
-
-
+                try {
+                    let txAttack =  await battleship.attackOpponent(matchId, i, j, { from: playerX });
+                    data.attackOpponent = txAttack.receipt.gasUsed;
+                    const merkleProof = genMerkleProof(board, i, j);
+                    const flatIndex = i * board.size + j;
+                    let txAttackProof = await battleship.submitAttackProof(matchId, String(board.cells[i][j]), String(merkleTreeLevels[0][flatIndex]), merkleProof, { from: playerY });
+                    data.submitAttackProof = txAttackProof.receipt.gasUsed;
+                    txAttack =  await battleship.attackOpponent(matchId, i, j, { from: playerY });
+                    data.attackOpponent = txAttack.receipt.gasUsed;
+                    txAttackProof = await battleship.submitAttackProof(matchId, String(board.cells[i][j]), String(merkleTreeLevels[0][flatIndex]), merkleProof, { from: playerX });
+                    data.submitAttackProof = txAttackProof.receipt.gasUsed;
+                } catch(error) {
+                    finish = true;
+                    break;
+                }
             }
         }
-
-
-
     });
 
     it("Player one send board for verification", async () => {
-
-      txVerification = await battleship.verifyBoard(matchId, board.cells.flat(), { from: playerX});
-      data.verifyBoard = txVerification.receipt.gasUsed;
-
-      //Check if the event is fired
-      truffleAssert.eventEmitted(txVerification, "winnerIs"); 
-
-
+        const txVerification = await battleship.verifyBoard(matchId, board.cells.flat(), { from: playerX});
+        data.verifyBoard = txVerification.receipt.gasUsed;
+        truffleAssert.eventEmitted(txVerification, "winnerIs"); 
     });
 
     
-    it("Save data to gas_total_cost.json", () => {
+    it("Gas cost", () => {
       const prev = JSON.parse(fs.readFileSync(gasFile));
       fs.writeFileSync(gasFile, JSON.stringify({ ...data, ...prev }));
     });
@@ -233,8 +205,6 @@ contract("Compute gas cost for a 8x8 board game", (accounts) => {
 function generateRandomSalt() {
     return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
   }
-
-
 
   function merkleTree(playerGrid, salt) {
     let boardForMT = playerGrid.cells.flat();
@@ -306,16 +276,10 @@ function genMerkleProof(playerGrid, row, col) {
 
 
 function xor(first, second) {
-  // Convert hexadecimal strings to BigInt
   const intermediate1 = BigInt(first);
   const intermediate2 = BigInt(second);
-
-  // Perform XOR operation and convert the result to a hexadecimal string
   const intermediate = (intermediate1 ^ intermediate2).toString(16);
-
-  // Prepend "0x" to the result string and ensure it has a length of 64 characters (32 bytes) by adding leading zeros if necessary
   const result = "0x" + intermediate.padStart(64, "0");
-
   // Return the resulting hexadecimal string
   return result;
 }
